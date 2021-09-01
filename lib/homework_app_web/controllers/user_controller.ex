69baa59540950886a340540d75f7804f287c36c2
Bruno Felipe <bruno.felipe7170@gmail.com>
2021-09-01 12:18:28 -0300

    Handling GET request to get user by id

--- lib/homework_app_web/controllers/user_controller.ex
+++ lib/homework_app_web/controllers/user_controller.ex
@@ -2,21 +2,21 @@ defmodule HomeworkAppWeb.UserController do
   use HomeworkAppWeb, :controller
   alias HomeworkApp.Schemas.User
 
-
-
-  def index(conn, _params) do
-    HomeworkApp.User.Get.index(conn)
-  end
-
   def show(conn, %{"id" => id}) do
-    HomeworkApp.User.Get.show(conn, id)
+    IO.inspect(id)
+
+    with {:ok, %User{} = user} <- HomeworkApp.get_user_by_id(id) do
+      conn
+      |> put_status(:ok)
+      |> render("user.json", user: user)
+    end
   end
 
   def create(conn, params) do
-
-    with {:ok, %User{} = user} <- HomeworkApp.User.Create.call_user(params) do
-        conn
-        |> put_status(:created)
-        |> render("create.json", user: user) end
+    with {:ok, %User{} = user} <- HomeworkApp.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
 end
--- lib/homework_app_web/views/user_view.ex
+++ lib/homework_app_web/views/user_view.ex
@@ -2,6 +2,12 @@ defmodule HomeworkAppWeb.UserView do
   use HomeworkAppWeb, :view
   alias HomeworkApp.Schemas.User
 
+  def render("user.json", %{user: %User{} = user}) do
+    %{
+      user: user
+    }
+  end
+
   def render("create.json", %{user: %User{} = user}) do
     %{
       message: "User created",
